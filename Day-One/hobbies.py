30afaa9c715dd4033037dc6468443601dbbc9c59
Jesus Akanle <jesuswrites20043@gmail.com>
2023-07-03 17:11:15 +0000

    -I added the backward slash to manage lines and arrangement

--- Day-One/hobbies.py
+++ Day-One/hobbies.py
@@ -2,10 +2,10 @@
 print("Welcome, Dear new User")
 
 # Ask the User for their name.
-User = input("What is your name?/n")
+User = input("What is your name?\n")
 
 # Ask the User for their hobby.
-Hobby = input("What is your hobby?/n")
+Hobby = input("What is your hobby?\n")
 
 # Send the result to the User.
 
