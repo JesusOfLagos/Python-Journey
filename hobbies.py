ce773d312fcacddfd3ed90f7e38b226895ab24c1
Jesus Akanle <jesuswrites20043@gmail.com>
2023-07-03 16:59:47 +0000

    I created a the print hobby function

--- hobbies.py
+++ hobbies.py
@@ -2,7 +2,11 @@
 print("Welcome, Dear new User")
 
 # Ask the User for their name.
-input("What is your name?")
+User = input("What is your name?/n")
 
 # Ask the User for their hobby.
-input("What is your hobby?")
+Hobby = input("What is your hobby?/n")
+
+# Send the result to the User.
+
+print("Dear" + User + "," + "Your Hobby is" + Hobby)
